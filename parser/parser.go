a33625a7603afee5d1839b4d3daa73938ee44303
phreppo <parolini.francesco.1@gmail.com>
2019-05-09 13:21:51 +0200

    tokenizer structure

--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,29 @@ package parser
 
 import . "github.com/parof/parallellisp/cell"
 
-func parse(source string) Cell {
-	return nil
+type tokenType int
+
+const (
+	tokNone  tokenType = 0
+	tokOpen  tokenType = 1
+	tokClose tokenType = 2
+	tokDot   tokenType = 3
+	tokQuote tokenType = 4
+	tokSym   tokenType = 5
+	tokNum   tokenType = 6
+	tokStr   tokenType = 7
+)
+
+type token struct {
+	typ tokenType
+	str string
+	val int
+}
+
+func Parse(source string) (Cell, bool, string) {
+	return nil, false, ""
+}
+
+func nextToken(source string) token {
+	return token{}
 }

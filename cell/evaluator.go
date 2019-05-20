150940029e681f5fb1639388a80072b8c21702e4
phreppo <parolini.francesco.1@gmail.com>
2019-05-20 18:31:07 +0200

    better eval result functions

--- cell/evaluator.go
+++ cell/evaluator.go
@@ -30,6 +30,14 @@ func newEvalResult(c Cell, e error) EvalResult {
 	return r
 }
 
+func newEvalPositiveResult(c Cell) EvalResult {
+	return newEvalResult(c, nil)
+}
+
+func newEvalErrorResult(e error) EvalResult {
+	return newEvalResult(nil, e)
+}
+
 type EvalRequest struct {
 	Cell      Cell
 	Env       *EnvironmentEntry
@@ -68,13 +76,13 @@ func serve(req EvalRequest) {
 
 func eval(toEval Cell, env *EnvironmentEntry) EvalResult {
 	if toEval == nil {
-		return newEvalResult(nil, nil)
+		return newEvalPositiveResult(nil)
 	}
 	switch c := toEval.(type) {
 	case *IntCell:
-		return newEvalResult(c, nil)
+		return newEvalPositiveResult(c)
 	case *StringCell:
-		return newEvalResult(c, nil)
+		return newEvalPositiveResult(c)
 	case *SymbolCell:
 		return assoc(c, env)
 	case *ConsCell:
@@ -89,14 +97,13 @@ func eval(toEval Cell, env *EnvironmentEntry) EvalResult {
 				argsResult = evlisSequential(c.Cdr, env)
 			}
 			if argsResult.Err != nil {
-				return newEvalResult(nil, argsResult.Err)
+				return newEvalErrorResult(argsResult.Err)
 			} else {
 				return apply(car, argsResult.Cell, env)
 			}
 		}
 	default:
-		error := newEvalError("[eval] Unknown cell type: " + fmt.Sprintf("%v", toEval))
-		return newEvalResult(nil, error)
+		return newEvalErrorResult(newEvalError("[eval] Unknown cell type: " + fmt.Sprintf("%v", toEval)))
 	}
 }
 
@@ -105,12 +112,12 @@ func evlisParallel(args Cell, env *EnvironmentEntry) EvalResult {
 	unvaluedArgs := extractCars(args)
 
 	if len(unvaluedArgs) == 0 {
-		return newEvalResult(nil, nil)
+		return newEvalPositiveResult(nil)
 	}
 
 	var replyChans []chan EvalResult
 	n := len(unvaluedArgs)
-	globalScheduler.addJobs(int32(n))
+	globalScheduler.addJobs(int32(n - 1))
 
 	for i := 0; i < n-1; i++ {
 		newChan := make(chan EvalResult)
@@ -132,13 +139,13 @@ func evlisParallel(args Cell, env *EnvironmentEntry) EvalResult {
 			evaluedArg := <-replyChans[i]
 			globalScheduler.jobEnded()
 			if evaluedArg.Err != nil {
-				return newEvalResult(nil, evaluedArg.Err)
+				return newEvalErrorResult(evaluedArg.Err)
 			}
 			appendCellToArgs(&top, &actCons, &(evaluedArg.Cell))
 		}
 	}
 
-	return newEvalResult(top, nil)
+	return newEvalPositiveResult(top)
 }
 
 func evlisSequential(args Cell, env *EnvironmentEntry) EvalResult {
@@ -194,21 +201,21 @@ func apply(function Cell, args Cell, env *EnvironmentEntry) EvalResult {
 		if Lisp.IsLambdaSymbol(functionCasted.Car) {
 			newEnv, err := pairlis(unsafeCadr(function), args, env)
 			if err != nil {
-				return newEvalResult(nil, err)
+				return newEvalErrorResult(err)
 			}
 			return eval(unsafeCaddr(function), newEnv)
 		} else {
 			// label check here
-			return newEvalResult(nil, newEvalError("[apply] trying to apply a non-lambda"))
+			return newEvalErrorResult(newEvalError("[apply] trying to apply a non-lambda"))
 		}
 	case *SymbolCell:
 		evaluedFunction := eval(function, env)
 		if evaluedFunction.Err != nil {
-			return newEvalResult(nil, evaluedFunction.Err)
+			return newEvalErrorResult(evaluedFunction.Err)
 		}
 		return apply(evaluedFunction.Cell, args, env)
 	default:
-		return newEvalResult(nil, newEvalError("[apply] trying to apply non-builtin, non-lambda, non-symbol"))
+		return newEvalErrorResult(newEvalError("[apply] trying to apply non-builtin, non-lambda, non-symbol"))
 	}
 }
 
@@ -235,19 +242,19 @@ func unsafeCaddr(c Cell) Cell {
 // Pre: symbol != nil, env. pair != nil
 func assoc(symbol *SymbolCell, env *EnvironmentEntry) EvalResult {
 	if res, isInGlobalEnv := GlobalEnv[symbol.Sym]; isInGlobalEnv {
-		return newEvalResult(res, nil)
+		return newEvalPositiveResult(res)
 	}
 	if env == nil {
-		return newEvalResult(nil, newEvalError("[assoc] symbol "+symbol.Sym+" not in env"))
+		return newEvalErrorResult(newEvalError("[assoc] symbol " + symbol.Sym + " not in env"))
 	}
 	act := env
 	for act != nil {
 		if (act.Pair.Symbol.Sym) == symbol.Sym {
-			return newEvalResult(act.Pair.Value, nil)
+			return newEvalPositiveResult(act.Pair.Value)
 		}
 		act = act.Next
 	}
-	return newEvalResult(nil, newEvalError("[assoc] symbol "+symbol.Sym+" not in env"))
+	return newEvalErrorResult(newEvalError("[assoc] symbol " + symbol.Sym + " not in env"))
 }
 
 func pairlis(formalParameters, actualParameters Cell, oldEnv *EnvironmentEntry) (*EnvironmentEntry, error) {

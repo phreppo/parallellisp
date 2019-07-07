53ffcd3d34f45f97733a2205c69803cd2a1c7263
phreppo <parolini.francesco.1@gmail.com>
2019-07-07 15:43:33 +0200

    cos

--- examples/run-fib.lisp
+++ examples/run-fib.lisp
@@ -1,36 +1,12 @@
 (load "pfib.lisp")
 
-(write "seq bench, seq fib")
-(time (bench fib 25))
-(write "")
-
-(write "seq bench, par fib")
-(time (bench p-fib 25))
-(write "")
-
-(write "par bench, seq fib")
-(time (p-bench fib 25))
-(write "")
-
-(write "par bench, par fib")
-(time (p-bench p-fib 25))
-(write "")
-
-(write "par bench, stupid par fib")
-(time (p-bench stupid-p-fib 25))
-
-;; ================================= Just fib =================================
-
-(write "fib 32")
+(write "Sequential fib 32")
 (time (fib 32))
 
-(write "pfib 32")
+(write "Naive pfib 32")
 (time (p-fib 32))
 
-;; it is crazy how keeping the biggest computation on the main thread could change performances
-;; ... or maybe no
-
-(write "library fib 32")
+(write "Optimal pfib 32")
 (time (
     parallelize 
         fib
@@ -41,18 +17,6 @@
         32 
 ))
 
-(write "inverted library fib 32")
-(time (
-    parallelize 
-        fib
-        (lambda (n) (or (eq n 0) (eq n 1)))
-        (lambda (n) (- n 2))
-        (lambda (n) (- n 1))
-        +
-        32 
-))
-
-;; questa cosa e completamente folle
 (setq pfib 
     (parallelize 
         fib
@@ -62,5 +26,5 @@
         +
     ))
 
-(write "closed fib 32")
+(write "Closed pfib 32")
 (time (pfib 32))

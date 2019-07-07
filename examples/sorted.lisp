c2961653bbebbb3e639e4ab47643950e697718b5
parof <parolini.francesco.1@gmail.com>
2019-07-07 15:14:16 +0200

    sorted now checks if the array is sorted

--- examples/sorted.lisp
+++ examples/sorted.lisp
@@ -5,7 +5,7 @@
         ((eq lst nil) t)
         ((eq (length lst) 1) t)
         ((eq (length lst) 2) 
-            (< (car lst) (car (cdr lst))))
+            (<= (car lst) (car (cdr lst))))
         (t
             (and 
                 (sorted (first-half lst))
